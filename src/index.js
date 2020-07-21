0a39ab4e5b34d38dc42f336c76a308ac15353dc0
Guosa_Edebiri <gedebiri@gmail.com>
2020-07-21 11:52:40 -0700

    didn't remember anything

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // write your code here
+
+
+const ul = document.getElementById('images');
+
+
+let images={};
+fetch('http://localhost:3000/images/')
+.then(response => response.json())
+.then(images => {
+      Object.keys(images).forEach((key) => {
+        const img = document.createElement("img");
+        img.setAttribute("src", images[key]);
+      });
+    })
+
